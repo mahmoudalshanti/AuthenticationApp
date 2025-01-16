3f2e0eb3533190e51772d2c3ae346e86e4332131
Medo Alshanti <medoalshanti@gmail.com>
2025-01-16 12:48:22 +0200

    last version

--- server-side/server.js
+++ server-side/server.js
@@ -25,10 +25,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client-side/dist")));
+  app.use(express.static(path.join(__dirname, "..", "/client-side/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client-side", "dist", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "..", "client-side", "dist", "index.html")
+    );
   });
 }
 
